fb6d221b4343d066f4813cff70e8b9a08e9002e2
anashrt <anashrt@users.noreply.github.com>
2020-01-07 23:01:30 +0000

    aaa

---
+++ src/main/java/com/openclassrooms/ideinstall/AAA.java
@@ -0,0 +1,9 @@
+package com.openclassrooms.ideinstall;
+
+public class AAA {
+
+	public AAA() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/main/java/com/openclassrooms/ideinstall/Test.java
+++ src/main/java/com/openclassrooms/ideinstall/Test.java
@@ -12,6 +12,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		
 		System.out.println("VAR = "+Test.VAR);
+		
 	}
 
 }
