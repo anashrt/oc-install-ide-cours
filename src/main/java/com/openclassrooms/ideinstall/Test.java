f15d7e402dd08156c48136a828b46b93d7e2df52
anashrt <anashrt@users.noreply.github.com>
2020-01-07 22:49:32 +0000

    ajouter fichier

---
+++ src/main/java/com/openclassrooms/ideinstall/Test.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.ideinstall;
+
+public class Test {
+	
+	public static int VAR = 5;
+
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("VAR = "+Test.VAR);
+	}
+
+}
